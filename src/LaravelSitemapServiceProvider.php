33c774d6b59bcd149ba65c1bc7ce70e09e2bccd5
Niclas Schirrmeister <nschirrmeister@elbgoods.de>
2021-03-08 14:27:23 +0100

    clean service provider

--- src/LaravelSitemapServiceProvider.php
+++ src/LaravelSitemapServiceProvider.php
@@ -12,40 +12,15 @@ class LaravelSitemapServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-sitemap');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-sitemap');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-sitemap.php'),
+                __DIR__.'/../config/config.php' => config_path('sitemap.php'),
             ], 'config');
 
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-sitemap'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-sitemap'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-sitemap'),
-            ], 'lang');*/
-
             // Registering package commands.
-            if ($this->app->runningInConsole()) {
-                $this->commands([
-                    GenerateCommand::class,
-                ]);
-            }
+            $this->commands([
+                GenerateCommand::class,
+            ]);
         }
     }
 
